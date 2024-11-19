412692ba7b54c936e0e4f4894d3fd973a1a20ad0
razmiqayelyan <raz.miqayelyan@gmail.com>
2024-11-18 18:04:59 -0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <button>FETCH</button>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Hooks/useFetch.jsx
@@ -0,0 +1,5 @@
+
+
+export function useFetch(url) {
+    
+}
